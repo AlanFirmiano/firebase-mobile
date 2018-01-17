e84bb990a17ff6c138933f8d3f6fa2baa71186cb
alanfirmiano <alanfirmiano18@gmail.com>
2018-01-17 15:33:28 -0300

    angular firebase e config.ts

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = var config = {
+  apiKey: "AIzaSyBntnqcdT8Vlnl685DsyhUxoERTwUVtbxU",
+  authDomain: "fir-auth-d190c.firebaseapp.com",
+  databaseURL: "https://fir-auth-d190c.firebaseio.com",
+  projectId: "fir-auth-d190c",
+  storageBucket: "",
+  messagingSenderId: "796000859261"
+};
